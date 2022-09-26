b6bfe1f28526176b55d46f79a8b3972928c10a53
Harish <Harish@DESKTOP-ARKBO9L>
2022-09-26 16:28:30 +0530

    add HelloJava.java

---
+++ CodeJourney/src/Java/HelloJava.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+
+}
--- JavaCodeJourney/src/module-info.java
+++ JavaCodeJourney/src/module-info.java
@@ -1,2 +1,4 @@
 module JavaCodeJourney {
 }
+
+//   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
