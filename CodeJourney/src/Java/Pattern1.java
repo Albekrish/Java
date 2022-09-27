18375380832e1211f36130c0303180cf4842900f
Albekrish <114362808+Albekrish@users.noreply.github.com>
2022-09-28 00:38:46 +0530

    Pattern1.java Pattern2.java RomanToInteger.java

---
+++ CodeJourney/src/Java/Pattern1.java
@@ -0,0 +1,146 @@
+package Java;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		pattern1(4);
+		System.out.println();
+		pattern2(4);
+		System.out.println();
+		pattern3(4);
+		System.out.println();
+		pattern4(4);
+		System.out.println();
+		pattern5(5);
+		System.out.println();
+		pattern6(5);
+		System.out.println();
+		pattern7(5);
+		System.out.println();
+		pattern8(5);
+		System.out.println();
+		pattern9(5);
+
+	}
+	public static void pattern1(int n) {    	
+    	
+    	for(int row=1;row<=n;row++) {
+    		for(int col=1;col<=n;col++) {
+    			System.out.print("* ");
+    		}
+    		System.out.println();
+    	}
+    }
+	
+    public static void pattern2(int n) {    	
+    	
+    	for(int row=1;row<=n;row++) {
+    		for(int col=1;col<=row;col++) {
+    			System.out.print("* ");
+    		}
+    		System.out.println();
+    	}
+    }
+    
+ public static void pattern3(int n) {    	
+    	
+    	for(int row=1;row<=n;row++) {
+    		for(int col=1;col<=n-row+1;col++) {
+    			System.out.print("* ");
+    		}
+    		
+    	//	OR  for(int col=n;col>=row;col--) {
+    	//		System.out.print("* ");
+    	//	}
+    		System.out.println();
+    	}
+    }
+ 
+ public static void pattern4(int n) {    	
+ 	
+ 	for(int row=1;row<=n;row++) {
+ 		for(int col=1;col<=row;col++) {
+ 			System.out.print(col+" ");
+ 		}
+ 		System.out.println();
+ 	}
+ }
+ 
+ public static void pattern5(int n) {    	
+	 	
+	 	for(int row=1;row<=(2*n)-1;row++) {
+	 		int totalCol= row>n ? (2*n-row):row;
+	 		for(int col=1;col<=totalCol;col++) {
+	 			System.out.print("* ");
+	 		}
+	 		System.out.println();
+	 	}
+	 }
+ 
+ public static void pattern6(int n) {    	
+	 	
+	 	for(int row=1;row<=n;row++) {
+	 		//space
+	 		for(int j=1;j<=n-row;j++) {
+	 			System.out.print("  ");
+	 		}
+	 		//star
+	 		for(int col=1;col<=row;col++) {
+	 			System.out.print("* ");
+	 		}
+	 		System.out.println();
+	 	}
+	 }
+ 
+ public static void pattern7(int n) {    	
+	 	
+	 	for(int row=1;row<=n;row++) {
+	 		//space
+	 		for(int j=1;j<=n-row;j++) {
+	 			System.out.print("  ");
+	 		}
+	 		//star
+	 		for(int col=1;col<=(2*row)-1;col++) {
+	 			System.out.print("* ");
+	 		}
+	 			 		
+	 		System.out.println();
+	 	}
+	 }
+ 
+ public static void pattern8(int n) {    	
+	 	
+	 	for(int row=1;row<=n;row++) {
+	 		//space
+	 		for(int j=1;j<=row-1;j++) {
+	 			System.out.print("  ");
+	 		}
+	 		//star
+	 		for(int col=1;col<=2*(n-row)+1;col++) {
+	 			System.out.print("* ");
+	 		}
+	 			 		
+	 		System.out.println();
+	 	}
+	 }
+ 
+ public static void pattern9(int n) {    	
+	 	for(int row=1;row<=2*n-1;row++) {
+	 		int totalCol= row>n ? 2*n-row:row;
+	 		
+	 		int noOfSpaces=n-totalCol;
+	 		
+	 		for(int s=1;s<=noOfSpaces;s++) {
+	 			System.out.print(" ");
+	 		}
+	 		for(int col=1;col<=totalCol;col++) {
+	 			System.out.print("* ");
+	 		}
+	 		System.out.println();
+	 	}
+	
+	 }
+  
+}
+
+
---
+++ CodeJourney/src/Java/Pattern2.java
@@ -0,0 +1,67 @@
+package Java;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		pattern1(4);
+		System.out.println();
+		pattern2(4);
+		System.out.println();
+		pattern3(4);
+	}
+	
+	public static void pattern1(int n) { 
+		
+		for(int row=1;row<=n;row++) {
+			//space
+			for(int s=1;s<=n-row;s++) {
+				System.out.print("  ");
+			}
+			
+			//no
+			for(int col=row;col>=1;col--) {
+				System.out.print(col+" ");
+			}
+			for(int col=2;col<=row;col++) {
+				System.out.print(col+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+public static void pattern2(int n) { 
+		
+		for(int row=1;row<=2*n-1;row++) {
+			int c= row>n ? 2*n-row:row;
+			//space
+			for(int s=1;s<=n-c;s++) {
+				System.out.print("  ");
+			}
+			
+			//no
+			for(int col=c;col>=1;col--) {
+				System.out.print(col+" ");
+			}
+			for(int col=2;col<=c;col++) {
+				System.out.print(col+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	public static void pattern3(int n) { 
+		int ori_n=n;
+		n=2*n-1;
+		for(int row=0;row<=n;row++) {		
+			for(int col=0;col<=n;col++) {
+			int index=ori_n-Math.min(Math.min(row, col), Math.min(n-row,n-col)); //find min (left,right,up,down)
+			System.out.print(index+" ");
+			}
+			System.out.println();
+		}
+	
+	}
+
+}
