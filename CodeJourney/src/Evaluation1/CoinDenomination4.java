3cf3be789794e20098611e61aff9a9fa7446851b
Albekrish <114362808+Albekrish@users.noreply.github.com>
2023-01-20 19:33:55 +0530

    Evaluation

--- CodeJourney/src/Evaluation1/CoinDenomination4.java
+++ CodeJourney/src/Evaluation1/CoinDenomination4.java
@@ -1,22 +1,52 @@
 package Evaluation1;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class CoinDenomination4 {
 
+
 	public static void main(String[] args) {
-		int total_amount=56;
+		int total_amount=56, sum=0;
+		boolean flag= true;
+		int coins[]= {5,5,5,5};
+		int value[]= {1,2,5,10};
 		
-		HashMap<Integer, Integer> hm= new HashMap<>();
-		//add coins
-		hm.put(1,5);
-		hm.put(2,5);
-		hm.put(5,5);
-		hm.put(10,5);
+		int remin= total_amount%value[value.length-1];
+		int del= total_amount/value[value.length-1];
+		int remain_added=0;		
 		
+		int total_coin=0;
+		if(remin==0) {
+			remain_added=Math.abs(total_coin-((del*value[value.length-1])-value[value.length-1]));
+			total_coin=del-1;
+		}else {
+			remain_added=Math.abs(total_amount -(del*value[value.length-1]));
+			total_coin=del;
+		}
 		
-		
-
+		for(int i=value.length-2;i>=0;i--) {
+			if(remain_added >= value[i]) {
+				remain_added -= value[i];
+				total_coin++;
+			}else {
+				continue;
+			}
+			if(remain_added <0) {
+				flag= false;
+				break;
+			}else if(remain_added ==0) {
+				flag=true;
+			}
+			
+		}
+		if(flag) {
+			System.out.println(total_coin);
+		}else {
+			System.out.println(-1);
+		}
 	}
-
 }
+
+
